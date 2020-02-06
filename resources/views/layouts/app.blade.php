f9c664c3c55304afdd0f5a56ed81ca336c7470d1
denisdalmeida04@gmail.com <denisdalmeida04@gmail.com>
2020-02-06 15:53:19 +0000

    first commit
    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width initial-scale=1.0">
+        <title>Adminca bootstrap 4 &amp; angular 5 admin template, Шаблон админки | Dashboard</title>
+        <!-- GLOBAL MAINLY STYLES-->
+        <link href="html/assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="html/assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+        <link href="html/assets/vendors/line-awesome/css/line-awesome.min.css" rel="stylesheet" />
+        <link href="html/assets/vendors/themify-icons/css/themify-icons.css" rel="stylesheet" />
+        <link href="html/assets/vendors/animate.css/animate.min.css" rel="stylesheet" />
+        <link href="html/assets/vendors/toastr/toastr.min.css" rel="stylesheet" />
+        <link href="html/assets/vendors/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet" />
+        <!-- PLUGINS STYLES-->
+        <link href="html/assets/vendors/jvectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" />
+        <!-- THEME STYLES-->
+        <link href="html/assets/css/main.min.css" rel="stylesheet" />
+        <!-- PAGE LEVEL STYLES-->
+    </head>
+<body>
+    
+@yield('content')
+
+    <!-- CORE PLUGINS-->
+    <script src="html/assets/vendors/jquery/dist/jquery.min.js"></script>
+    <script src="html/assets/vendors/popper.js/dist/umd/popper.min.js"></script>
+    <script src="html/assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="html/assets/vendors/metisMenu/dist/metisMenu.min.js"></script>
+    <script src="html/assets/vendors/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="html/assets/vendors/jquery-idletimer/dist/idle-timer.min.js"></script>
+    <script src="html/assets/vendors/toastr/toastr.min.js"></script>
+    <script src="html/assets/vendors/jquery-validation/dist/jquery.validate.min.js"></script>
+    <script src="html/assets/vendors/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+    <!-- PAGE LEVEL PLUGINS-->
+    <script src="html/assets/vendors/chart.js/dist/Chart.min.js"></script>
+    <script src="html/assets/vendors/jquery.easy-pie-chart/dist/jquery.easypiechart.min.js"></script>
+    <script src="html/assets/vendors/jvectormap/jquery-jvectormap-2.0.3.min.js"></script>
+    <script src="html/assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <!-- CORE SCRIPTS-->
+    <script src="html/assets/js/app.min.js"></script>
+    <!-- PAGE LEVEL SCRIPTS-->
+    <script src="html/assets/js/scripts/dashboard_visitors.js"></script>
+</body>
+</html>
