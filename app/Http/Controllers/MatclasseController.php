ffc58c981271c11b7c13ac034076aeda524f29be
pascal0101 <pascaldaf2016@gmail.com>
2020-03-17 12:04:30 +0100

    création des models

---
+++ app/Http/Controllers/MatclasseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mateclass;
+use Illuminate\Http\Request;
+
+class MatclasseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Mateclass  $mateclass
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Mateclass $mateclass)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Mateclass  $mateclass
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Mateclass $mateclass)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Mateclass  $mateclass
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Mateclass $mateclass)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Mateclass  $mateclass
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Mateclass $mateclass)
+    {
+        //
+    }
+}
